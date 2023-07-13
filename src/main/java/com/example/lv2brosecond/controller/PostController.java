8ea6fb4b48b36e7c541d820d91456915d32d2b2e
LOGOS125-GALAXY\qkrtj <qkrtjdrbs125@naver.com>
2023-07-13 20:56:36 +0900

    이게뭐지?

--- src/main/java/com/example/lv2brosecond/controller/PostController.java
+++ src/main/java/com/example/lv2brosecond/controller/PostController.java
@@ -14,9 +14,10 @@ import java.util.List;
 public class PostController {
 
     private final PostService postService;
-    public PostController(PostService postService){
-        this.postService = postService;
-    }
+    public PostController(PostService postService){//생성자부분:값을 초기화시켜주는것;외부에있는 PostService를 가져와서 초기화시켜준다.
+        this.postService = postService;//이부분은 무조건 이해하고있어야한다. ioc di부분은 필수!!!
+    } //1주차때 ioc di 부분 강좌를 다시보면 된다. 강의에 자세하게 나와있다. - 자료에// 생성자를 통해 주입을 받고있다.
+//    PostService postService = new PostService(); //직접 객체를 만드는것 우리가 만든거라 관리에대한 책임이 우리한테있다.
 
     @PostMapping("/post")
     public PostResponseDto createPost(@RequestBody PostRequestDto requestDto, HttpServletRequest httpServletRequest) {
